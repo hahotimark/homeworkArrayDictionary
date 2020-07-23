ed02d51cf36933439043c554aee0e264f8c114b0
Márk <hahoti.mark@ekreta.hu>
2020-07-23 12:23:50 +0200

    szotar

---
+++ arrayDictionary/src/Dictionary.java
@@ -0,0 +1,29 @@
+import java.lang.reflect.Array;
+
+public class Dictionary {
+
+    String[][] words;
+
+    public Dictionary() {
+
+        words = new String[20][20];
+    }
+
+    public void learn(String w) {
+
+        words.toString();
+
+    }
+
+    public void translate(String word){
+
+        for (int x = 0; x < words[x].length; x++){
+
+            for (int y = 0; y < words[y].length; y++) {
+
+                words.toString();
+            }
+        }
+
+    }
+}
